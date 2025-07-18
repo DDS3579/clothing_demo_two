03ac02d5268d711c7e3c4e8524a30ec1f40b5224
Divya <divyadarsheel.dds@gmail.com>
2025-07-18 15:20:20 +0545

    Updated for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/clothing_demo_two/',
   server: {
     host: "::",
     port: 8080,
